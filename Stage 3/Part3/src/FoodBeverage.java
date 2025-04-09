7ad1d4849b4222cc6746673d48696e5c4d073963
Paola261 <peredapaola21@gmail.com>
2025-04-08 22:21:32 -0600

    Created the FoodBeverage Class

---
+++ Stage 3/Part3/src/FoodBeverage.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author paolapereda
+ */
+public class FoodBeverage {
+    
+    private String size;
+    
+}
