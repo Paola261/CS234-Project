e3168e2f8b01fd8b45214359d75a046325b33c54
Paola261 <peredapaola21@gmail.com>
2025-04-08 22:33:01 -0600

    Updated FoodBeverage Class

--- Stage 3/Part3/src/FoodBeverage.java
+++ Stage 3/Part3/src/FoodBeverage.java
@@ -7,8 +7,58 @@
  *
  * @author paolapereda
  */
+import java.util.ArrayList;
+
 public class FoodBeverage {
-    
+
+    private String itemName; 
+    private double price;
     private String size;
+    private String snacks;
+    private String drink;
+
+    private ArrayList<String> foodItems;
+
+    public FoodBeverage()
+    {
+        // this.itemName = itemName;
+        // this.price = price;
+        // this.size = size;
+        foodItems = new ArrayList<>();
+    }
+
+    public void addFoodItem(String item)
+    {
+        foodItems.add(item);
+    }
+
+    public String getItemName()
+    {
+        return itemName;
+    }
+
+    public double getPrice()
+    {
+        return price;
+    }
+
+    public String getSize()
+    {
+        return size;
+    }
+
+    public String[] getOrder()
+    {
+        ArrayList<String> orderDetails = new ArrayList<>();
+        orderDetails.add("Size: " + size);
+        orderDetails.add("Drink: " + drink);
+        orderDetails.add("Snacks: " + snacks);
+        orderDetails.add("Price:  $" + price);
+        orderDetails.addAll(foodItems);
+
+        return orderDetails.toArray(new String[0]);
+    }
+
     
 }
+
