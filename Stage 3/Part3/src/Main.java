a1cc247348afbecd8396fa947ae7c3492920e1b4
tjpiz <tjpiz@TarynsLaptop.home>
2025-04-30 09:55:23 -0600

    updated main method to fix errors

--- Stage 3/Part3/src/Main.java
+++ Stage 3/Part3/src/Main.java
@@ -149,6 +149,10 @@ public class Main {
         String customerInfo = "customers.csv";
         new Customer_GUI(aPerson, customerInfo).setVisible(true);
        
+        /* POSSIBLE SOLUTION TO ABOVE CODE
+        Customer aPerson = new Customer("Name", 999, (byte)25);
+        new Customer_GUI(aPerson, "customers.csv").setVisible(true);
+        */
         boolean running = true;
         while (running) {
             System.out.println("\n Welcome to the Movie Theater System");
@@ -333,8 +337,7 @@ public class Main {
                     } else {
                         System.out.println("Staff ID not found");
                     }
-                    break;
-     '
+                    break;   
                 
                 case "10":
                     loggedIn = false;
